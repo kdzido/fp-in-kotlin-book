7006ab2839c219341be6e039c418f4e65811d1a2
kdzido <krzysztof.dzido@gmail.com>
2024-05-28 23:19:05 +0200

    Establish Gradle build

---
+++ src/main/kotlin/funkotlin/fp_in_kotlin_book/FpInKotlinBookApplication.kt
@@ -0,0 +1,11 @@
+package funkotlin.fp_in_kotlin_book
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FpInKotlinBookApplication
+
+fun main(args: Array<String>) {
+	runApplication<FpInKotlinBookApplication>(*args)
+}
---
+++ src/test/kotlin/funkotlin/fp_in_kotlin_book/FpInKotlinBookApplicationTests.kt
@@ -0,0 +1,13 @@
+package funkotlin.fp_in_kotlin_book
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FpInKotlinBookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
