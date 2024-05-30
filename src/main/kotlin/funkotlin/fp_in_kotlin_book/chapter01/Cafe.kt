8f754eb88ecb1c3cbf73868365e0fdd539e2a8fa
kdzido <krzysztof.dzido@gmail.com>
2024-05-30 20:26:40 +0200

    [CHAP-01][LST] Listing 1.4

--- src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter01/Cafe.kt
+++ src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter01/Cafe.kt
@@ -1,16 +1,25 @@
 package funkotlin.fp_in_kotlin_book.chapter01
 
-// Listing 1.2
+// Listing 1.3
 class Cafe {
-    fun buyCoffee(cc: CreditCard, p: Payments): Coffee {
+    fun buyCoffee(cc: CreditCard, p: Payments): Pair<Coffee, Charge> {
         val cup = Coffee()
-        p.charge(cc, cup.price)
-        return cup
+        return Pair(cup, Charge(cc, cup.price))
     }
 }
 
 class Coffee {
-    val price: Price = Price(0.0)
+    val price: Float = 0.0F
+}
+
+// Listing 1.4 charge as data type
+data class Charge(val cc: CreditCard, val amount: Float) {
+    fun combine(other: Charge): Charge =
+        if (cc == other.cc)
+            Charge(cc, amount + other.amount)
+        else throw Exception(
+            "Cannot combine charges to different cards"
+        )
 }
 
 interface Payments {
@@ -19,4 +28,4 @@ interface Payments {
 
 interface CreditCard
 
-data class Price(val d: Double)
+data class Price(val d: Float)
