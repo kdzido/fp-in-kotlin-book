f3c470a1844e87177ca9fa017d9bd1bb6c11b27c
kdzido <krzysztof.dzido@gmail.com>
2024-08-31 19:27:43 +0200

    [CHAP-03][LST-03] singly-linked list

--- src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter03/Chatper03.kt
+++ src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter03/Chatper03.kt
@@ -1,13 +1,20 @@
 package funkotlin.fp_in_kotlin_book.chapter03
 
-sealed class List<out A>
+sealed class List<out A> {
+    companion object {
+        fun <A> of(vararg aa: A): List<A> {
+            val tail = aa.sliceArray(1 until aa.size)
+            return if (aa.isEmpty()) Nil else Cons(aa[0], of(*tail))
+        }
+    }
+}
+
 object Nil : List<Nothing>() {
     override fun toString(): String {
         return "Nil"
     }
 }
 data class Cons<out A>(val head: A, val tail: List<A>) : List<A>()
-
 
 
 fun main() {
@@ -20,4 +27,5 @@ fun main() {
     println("l1: " + ex1)
     println("l2: " + ex2)
     println("l3: " + ex3)
+    println("of: " + List.of(1,2,3))
 }
