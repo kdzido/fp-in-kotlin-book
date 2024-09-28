53f0622310de16eea00c86c5085ff543dd49f175
kdzido <krzysztof.dzido@gmail.com>
2024-09-28 23:13:55 +0200

    [CHAP-04][LST-4.8] either with map2

--- src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter04/Chapter04Either.kt
+++ src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter04/Chapter04Either.kt
@@ -100,6 +100,25 @@ suspend fun String.parseToInt(): arrow.core.Either<Throwable, Int> =
     arrow.core.Either.catch { this.toInt() }
 
 
+data class Name(val value: String)
+data class Age(val value: Int)
+data class Person(val name: Name, val age: Age)
+
+fun mkName(name: String): Either<String, Name> =
+    if (name.isBlank()) Left("Name is empty")
+    else Right(Name(name))
+
+fun mkAge(age: Int): Either<String, Age> =
+    if (age < 0) Left("Age is out of range")
+    else Right(Age(age))
+
+fun mkPerson(name: String, age: Int): Either<String, Person> =
+    map2E(mkName(name), mkAge(age), {n, a -> Person(n, a) })
+
 fun main() {
-    print("Either")
+    println("Either - main")
+
+    println(mkPerson("John", 10))
+    println(mkPerson("John", -1))
+    println(mkPerson("", 10))
 }
