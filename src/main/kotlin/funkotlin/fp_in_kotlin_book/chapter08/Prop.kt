e41c7c824e2450af57020f726f3bdc4db4c75936
kdzido <krzysztof.dzido@gmail.com>
2025-05-02 13:23:27 +0200

    [CHAP-08][LST-8.2] RNG, State, Gen

--- src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter08/Prop.kt
+++ src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter08/Prop.kt
@@ -1,15 +1,34 @@
 package funkotlin.fp_in_kotlin_book.chapter08
 
-interface Gen<A>
+import arrow.core.Either
+
+interface RNG {
+    fun nextInt(): Pair<Int, RNG>
+}
+
+data class State<S, out A>(val run: (S) -> Pair<A, S>)
+
+data class Gen<A>(val sample: State<RNG, A>)
+
+typealias SuccessCount = Int
+typealias FailedCase = String
 
 interface Prop {
-    fun check(): Boolean
+    fun check(): Either<Pair<FailedCase, SuccessCount>, SuccessCount>
 
     fun add(p: Prop): Prop {
         val outer = this
         return object : Prop {
-            override fun check(): Boolean {
-                return p.check() && outer.check()
+            override fun check(): Either<Pair<FailedCase, SuccessCount>, SuccessCount> {
+                val p1 = outer.check()
+                val p2 = p.check()
+                return when {
+                    p1 is Either.Right && p2 is Either.Right -> Either.right(2)
+                    p1 is Either.Left && p2 is Either.Right -> Either.left(Pair(p1.a.first, 1))
+                    p1 is Either.Right && p2 is Either.Left -> Either.left(Pair(p2.a.first, 1))
+                    p1 is Either.Left && p2 is Either.Left -> Either.left(Pair(p1.a.first + p1.a.first, 0))
+                    else -> throw IllegalStateException("Not possible")
+                }
             }
         }
     }
