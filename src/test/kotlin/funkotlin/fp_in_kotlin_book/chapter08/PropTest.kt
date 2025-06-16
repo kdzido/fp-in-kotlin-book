7dfc17848a5ab05bf1e2f78e618eaca7ff764a4b
kdzido <krzysztof.dzido@gmail.com>
2025-06-16 19:59:23 +0200

    [CHAP-08][LST-8.12c] takeWhile prop

--- src/test/kotlin/funkotlin/fp_in_kotlin_book/chapter08/PropTest.kt
+++ src/test/kotlin/funkotlin/fp_in_kotlin_book/chapter08/PropTest.kt
@@ -2,11 +2,13 @@ package funkotlin.fp_in_kotlin_book.chapter08
 
 import arrow.core.extensions.list.foldable.exists
 import arrow.core.extensions.list.foldable.firstOption
+import arrow.core.extensions.list.foldable.forAll
 import arrow.core.lastOrNone
 import funkotlin.fp_in_kotlin_book.chapter06.SimpleRNG
 import funkotlin.fp_in_kotlin_book.chapter07.Par
 import funkotlin.fp_in_kotlin_book.chapter07.Pars
 import funkotlin.fp_in_kotlin_book.chapter07.Pars.map
+import funkotlin.fp_in_kotlin_book.chapter08.Gen.Companion.listOfN
 import funkotlin.fp_in_kotlin_book.chapter08.Prop.Companion.forAll
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
@@ -53,7 +55,7 @@ class PropTest : StringSpec({
 
     "Max of list" {
         val rng = SimpleRNG(1L)
-        val intList = Gen.listOfN(Gen.choose(1, 10), Gen.choose(0, 100))
+        val intList = listOfN(Gen.choose(1, 10), Gen.choose(0, 100))
 
         forAll(intList) { ls ->
             ls.reversed().max() == ls.max()
@@ -120,6 +122,16 @@ class PropTest : StringSpec({
     "should run check" {
         Prop.run(Prop.check({ true })).shouldBeInstanceOf<Proved>()
         Prop.run(Prop.check({ false })).shouldBeInstanceOf<Falsified>()
+    }
+
+    "should takeWhile prop" {
+        val isEven: (Int) -> Boolean = { i: Int -> i % 2 == 0 }
+        val n = Gen.choose(0, 10)
+        val ga = Gen.choose(0, 100)
+        val takeWhileProp = forAll(listOfN(n, ga)) { ns ->
+            ns.takeWhile(isEven).forAll(isEven)
+        }
+        Prop.run(takeWhileProp)
     }
 })
 
