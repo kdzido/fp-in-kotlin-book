903f69bd2ebdcd7abb72a04c933e4a64c1d40047
kdzido <krzysztof.dzido@gmail.com>
2025-07-03 23:55:53 +0200

    [CHAP-10][EXER-10.1] monoids for ints add+multi, bools and+or

--- src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter10/Monoid.kt
+++ src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter10/Monoid.kt
@@ -14,3 +14,22 @@ fun <A> listMonoid(): Monoid<List<A>> = object : Monoid<List<A>> {
     override fun combine(a1: List<A>, a2: List<A>): List<A> = a1 + a2
     override val nil: List<A> = emptyList()
 }
+
+fun intAddition(): Monoid<Int> = object : Monoid<Int> {
+    override fun combine(a1: Int, a2: Int): Int = a1 + a2
+    override val nil: Int = 0
+}
+
+fun intMultiplication(): Monoid<Int> = object : Monoid<Int> {
+    override fun combine(a1: Int, a2: Int): Int = a1 * a2
+    override val nil: Int = 1
+}
+fun booleanOr(): Monoid<Boolean> = object : Monoid<Boolean> {
+    override fun combine(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+    override val nil: Boolean = false
+}
+
+fun booleanAnd(): Monoid<Boolean> = object : Monoid<Boolean> {
+    override fun combine(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+    override val nil: Boolean = true
+}
--- src/test/kotlin/funkotlin/fp_in_kotlin_book/chapter10/MonoidTest.kt
+++ src/test/kotlin/funkotlin/fp_in_kotlin_book/chapter10/MonoidTest.kt
@@ -13,4 +13,42 @@ class MonoidTest : StringSpec({
         listMonoid<String>().combine(listOf("one"), listOf("two")) shouldBe listOf("one", "two")
         listMonoid<String>().combine(listOf("one"), listMonoid<String>().nil) shouldBe listOf("one")
     }
+
+    "ints add monoid" {
+        val addMonoid = intAddition()
+        val nil = addMonoid.nil
+        addMonoid.combine(2, 3) shouldBe 5
+        addMonoid.combine(2, nil) shouldBe 2
+    }
+
+    "ints multi monoid" {
+        val mulMonoid = intMultiplication()
+        val nil = mulMonoid.nil
+        mulMonoid.combine(2, 3) shouldBe 6
+        mulMonoid.combine(2, nil) shouldBe 2
+    }
+
+    "boolean or monoid" {
+        val orMonoid = booleanOr()
+        val nil = orMonoid.nil
+        orMonoid.combine(true, true) shouldBe true
+        orMonoid.combine(true, false) shouldBe true
+        orMonoid.combine(false, true) shouldBe true
+        orMonoid.combine(false, false) shouldBe false
+        // and
+        orMonoid.combine(false, nil) shouldBe false
+        orMonoid.combine(true, nil) shouldBe true
+    }
+
+    "boolean and monoid" {
+        val andMonoid = booleanAnd()
+        val nil = andMonoid.nil
+        andMonoid.combine(true, true) shouldBe true
+        andMonoid.combine(true, false) shouldBe false
+        andMonoid.combine(false, true) shouldBe false
+        andMonoid.combine(false, false) shouldBe false
+        // and
+        andMonoid.combine(false, nil) shouldBe false
+        andMonoid.combine(true, nil) shouldBe true
+    }
 })
