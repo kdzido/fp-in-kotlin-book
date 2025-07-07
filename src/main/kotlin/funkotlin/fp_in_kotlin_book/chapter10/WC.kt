2712f52178c2f08c01b0e35b8811d9f121abfeb8
kdzido <krzysztof.dzido@gmail.com>
2025-07-07 21:57:56 +0200

    [CHAP-10][EXER-10.10] word count monoid

--- src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter10/WC.kt
+++ src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter10/WC.kt
@@ -2,5 +2,25 @@ package funkotlin.fp_in_kotlin_book.chapter10
 
 sealed class WC
 
-data class Stub(val chars: String)
-data class Part(val ls: String, val words: Int, val rs: String)
+data class Stub(val chars: String) : WC()
+data class Part(val ls: String, val words: Int, val rs: String) : WC()
+
+val wordCount: Monoid<WC> = object : Monoid<WC> {
+    override fun combine(a1: WC, a2: WC): WC =
+        when {
+            a1 is Stub && a2 is Stub ->
+                Stub(a1.chars + a2.chars)
+            a1 is Stub && a2 is Part ->
+                Part(a1.chars + a2.ls, a2.words, a2.rs)
+            a1 is Part && a2 is Stub ->
+                Part(a1.ls, a1.words, a1.rs + a2.chars)
+            a1 is Part && a2 is Part ->
+                Part(a1.ls, a1.words + isThereAdditionalWord(a1.rs, a2.ls) + a2.words, a2.rs)
+            else -> TODO("Not supported combination")
+        }
+
+    override val nil: WC get() = Stub("")
+}
+
+private fun isThereAdditionalWord(rs: String, ls: String): Int =
+    if ((rs + ls).isEmpty()) 0 else 1
--- src/test/kotlin/funkotlin/fp_in_kotlin_book/chapter10/WCTest.kt
+++ src/test/kotlin/funkotlin/fp_in_kotlin_book/chapter10/WCTest.kt
@@ -1,12 +1,34 @@
 package funkotlin.fp_in_kotlin_book.chapter10
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
 class WCTest : StringSpec({
+
+    "WC monoid" {
+        val wcMonoid = wordCount
+        val nil = wcMonoid.nil
+
+        // expect: id law
+        wcMonoid.combine(Stub(""), nil) shouldBe Stub("")
+        wcMonoid.combine(Stub(", "), nil) shouldBe Stub(", ")
+        wcMonoid.combine(Stub("abc"), nil) shouldBe Stub("abc")
+        wcMonoid.combine(nil, Stub("abc")) shouldBe Stub("abc")
+        wcMonoid.combine(Part("lorem", 1, "do"), nil) shouldBe Part("lorem", 1, "do")
+        wcMonoid.combine(nil, Part("lorem", 1, "do")) shouldBe Part("lorem", 1, "do")
+        // and:
+        wcMonoid.combine(Part("lorem", 1, "do"), Stub("able")) shouldBe Part("lorem", 1, "doable")
+        wcMonoid.combine(Part("lorem", 1, "do"), Part("able", 0, "end")) shouldBe Part("lorem", 2, "end")
+    }
+
+    "should count over" {
+//        countWords("lorem ipsum do") shouldBe
+//                Part("lorem", 1, "do")
+//        countWords("lor sit amet, ") shouldBe
+//                Part("lor", 2, "") // comma discareded
+    }
+
     "should parse chunks in parallel" {
         val largeText = "lorem ipsum dolor sit amet, "
-
-
-
     }
 })
