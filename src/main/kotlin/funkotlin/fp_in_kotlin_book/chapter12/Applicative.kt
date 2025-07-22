0b5198129e34df6d276242820f42fe459cec0971
kdzido <krzysztof.dzido@gmail.com>
2025-07-22 20:25:28 +0200

    [CHAP-12][EXER-12.3] map3, map4

--- src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter12/Applicative.kt
+++ src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter12/Applicative.kt
@@ -1,6 +1,7 @@
 package funkotlin.fp_in_kotlin_book.chapter12
 
 import arrow.Kind
+import arrow.core.curry
 import funkotlin.fp_in_kotlin_book.chapter03.Cons
 import funkotlin.fp_in_kotlin_book.chapter03.List as ListL
 import funkotlin.fp_in_kotlin_book.chapter11.Functor
@@ -26,6 +27,32 @@ interface Applicative<F> : Functor<F> {
         val fCurried: (A) -> (B) -> C = { a -> { b -> f(a, b) } }
         val l1: Kind<F, (B) -> C> = apply(unit(fCurried), fa)
         return apply(l1, fb)
+    }
+
+    fun <A, B, C, D> map3(
+        fa: Kind<F, A>,
+        fb: Kind<F, B>,
+        fc: Kind<F, C>,
+        f: (A, B, C) -> D,
+    ): Kind<F, D> {
+        val fCurried: (A) -> (B) -> (C) -> D = { a -> { b -> { c -> f(a, b, c) } } }
+        val l1: Kind<F, (B) -> (C) -> D> = apply(unit(fCurried), fa)
+        val l2: Kind<F, (C) -> D> = apply(l1, fb)
+        return apply(l2, fc)
+    }
+
+    fun <A, B, C, D, E> map4(
+        fa: Kind<F, A>,
+        fb: Kind<F, B>,
+        fc: Kind<F, C>,
+        fd: Kind<F, D>,
+        f: (A, B, C, D) -> E,
+    ): Kind<F, E> {
+        val fCurried: (A) -> (B) -> (C) -> (D) -> E = { a -> { b -> { c -> { d -> f(a, b, c, d) } } } }
+        val l1: Kind<F, (B) -> (C) -> (D) -> E> = apply(unit(fCurried), fa)
+        val l2: Kind<F, (C) -> (D) -> E> = apply(l1, fb)
+        val l3: Kind<F, (D) -> E> = apply(l2, fc)
+        return apply(l3, fd)
     }
 
     fun <A, B> traverse(
