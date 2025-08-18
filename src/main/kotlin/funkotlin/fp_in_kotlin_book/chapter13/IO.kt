9591a686e965eb8fec7a1e2a3bf45530dac8f159
kdzido <krzysztof.dzido@gmail.com>
2025-08-18 17:12:58 +0200

    [CHAP-13][LST-13.2] separated the logic and the console effect

--- src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter13/IO.kt
+++ src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter13/IO.kt
@@ -1,12 +1,26 @@
 package funkotlin.fp_in_kotlin_book.chapter13
 
+import funkotlin.fp_in_kotlin_book.chapter04.None
+import funkotlin.fp_in_kotlin_book.chapter04.Option
+import funkotlin.fp_in_kotlin_book.chapter04.Some
+
+fun main() {
+    val p1 = Player("Joe", 3)
+    val p2 = Player("Adam", 4)
+    contest(p1, p2)
+}
+
 data class Player(val name: String, val score: Int)
 
-fun contest(p1: Player, p2: Player): Unit = when {
-    p1.score > p2.score ->
-        println("${p1.name} is the winner!")
-    p1.score < p2.score ->
-        println("${p2.name} is the winner!")
-    else ->
+fun contest(p1: Player, p2: Player): Unit = when (val player = winner(p1, p2)) {
+    is Some ->
+        println("${player.value.name} is the winner!")
+    is None ->
         println("It's a draw!")
+}
+
+fun winner(p1: Player, p2: Player): Option<Player> = when {
+    p1.score > p2.score -> Some(p1)
+    p1.score < p2.score -> Some(p2)
+    else -> None
 }
