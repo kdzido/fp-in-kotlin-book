2512e1e2f130546c0e5b8c9ecca8841e46cf740d
kdzido <krzysztof.dzido@gmail.com>
2025-08-18 18:33:20 +0200

    [CHAP-13][LST-13.2] separated the logic and the console effect - PART 2

--- src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter13/IO.kt
+++ src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter13/IO.kt
@@ -4,19 +4,38 @@ import funkotlin.fp_in_kotlin_book.chapter04.None
 import funkotlin.fp_in_kotlin_book.chapter04.Option
 import funkotlin.fp_in_kotlin_book.chapter04.Some
 
-fun main() {
-    val p1 = Player("Joe", 3)
-    val p2 = Player("Adam", 4)
-    contest(p1, p2)
+interface IO {
+    companion object {
+        fun empty(): IO = object : IO {
+            override fun run(): Unit = Unit
+        }
+    }
+    fun run(): Unit
+
+    fun assoc(io: IO): IO = object : IO {
+        override fun run() {
+            this@IO.run()
+            io.run()
+        }
+    }
 }
 
 data class Player(val name: String, val score: Int)
 
-fun contest(p1: Player, p2: Player): Unit = when (val player = winner(p1, p2)) {
-    is Some ->
-        println("${player.value.name} is the winner!")
-    is None ->
-        println("It's a draw!")
+fun contest2(p1: Player, p2: Player): IO =
+    stdout(winnerMsg(winner(p1, p2)))
+
+fun stdout(msg: String): IO =
+    object : IO {
+        override fun run() = println(msg)
+    }
+
+fun contest(p1: Player, p2: Player): Unit =
+    println(winnerMsg(winner(p1, p2)))
+
+fun winnerMsg(p: Option<Player>): String = when (p) {
+    None -> "It's a draw!"
+    is Some -> "${p.value.name} is the winner!"
 }
 
 fun winner(p1: Player, p2: Player): Option<Player> = when {
---
+++ src/test/kotlin/funkotlin/fp_in_kotlin_book/chapter13/IOTest.kt
@@ -0,0 +1,22 @@
+package funkotlin.fp_in_kotlin_book.chapter13
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+
+class IOTest : StringSpec({
+
+    "contest should print declared winner" {
+        val p1 = Player("Joe", 3)
+        val p2 = Player("Adam", 4)
+
+        contest(p1, p2)
+    }
+
+    "contest2 should print declared winner" {
+        val p1 = Player("Joe", 3)
+        val p2 = Player("Adam", 4)
+
+        contest2(p1, p2).run()
+    }
+})
