94704e148feea9df4eace93067895c1472615ea9
kdzido <krzysztof.dzido@gmail.com>
2025-08-18 21:55:22 +0200

    [CHAP-13][LST-13.4] input IO effect

--- src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter13/IO.kt
+++ src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter13/IO.kt
@@ -4,31 +4,42 @@ import funkotlin.fp_in_kotlin_book.chapter04.None
 import funkotlin.fp_in_kotlin_book.chapter04.Option
 import funkotlin.fp_in_kotlin_book.chapter04.Some
 
-interface IO {
+interface IO<A> {
     companion object {
-        fun empty(): IO = object : IO {
-            override fun run(): Unit = Unit
+        fun <A> unit(a: () -> A) = object : IO<A> {
+            override fun run(): A = a()
         }
-    }
-    fun run(): Unit
 
-    fun assoc(io: IO): IO = object : IO {
-        override fun run() {
-            this@IO.run()
-            io.run()
-        }
+        operator fun <A> invoke(a: () -> A) = unit(a)
     }
+
+    fun run(): A
+
+    fun <B> map(f: (A) -> B): IO<B> =
+        object : IO<B> {
+            override fun run(): B = f(this@IO.run())
+        }
+
+    fun <B> flatMap(f: (A) -> IO<B>): IO<B> =
+        object : IO<B> {
+            override fun run(): B = f(this@IO.run()).run()
+        }
+
+    infix fun <B> assoc(io: IO<B>): IO<Pair<A, B>> =
+        object : IO<Pair<A, B>> {
+            override fun run(): Pair<A, B> =
+                this@IO.run() to io.run()
+        }
 }
 
 data class Player(val name: String, val score: Int)
 
-fun contest2(p1: Player, p2: Player): IO =
+fun contest2(p1: Player, p2: Player): IO<Unit> =
     stdout(winnerMsg(winner(p1, p2)))
 
-fun stdout(msg: String): IO =
-    object : IO {
-        override fun run() = println(msg)
-    }
+fun stdin(): IO<String> = IO { readLine().orEmpty() }
+
+fun stdout(msg: String): IO<Unit> = IO { println(msg) }
 
 fun contest(p1: Player, p2: Player): Unit =
     println(winnerMsg(winner(p1, p2)))
@@ -46,12 +57,32 @@ fun winner(p1: Player, p2: Player): Option<Player> = when {
 
 fun farenheitToCelsius(f: Double): Double = (f - 32) * 5.0 / 9.0
 
+@Deprecated("Use converter2")
 fun converter() {
     println("Enter a temperature in degrees Farenheit: ")
     val d = readLine().orEmpty().toDouble()
     println(farenheitToCelsius(d))
 }
 
+fun converter2(): IO<Unit> =
+    stdout("Enter a temperature in degrees Farenheit: ").flatMap {
+        stdin().map { it.toDouble() }.flatMap { df ->
+            stdout("Degrees Celsius: ${farenheitToCelsius(df)}")
+        }
+    }
+
+val echo: IO<Unit> = stdin().flatMap(::stdout)
+val readInt: IO<Int> = stdin().map { it.toInt() }
+val readInts: IO<Pair<Int, Int>> = readInt assoc readInt
+
 fun main() {
-    converter()
+//    converter()
+    converter2().run()
+
+//    println("echo: ")
+//    echo.run()
+//    println("readInt: ")
+//    readInt.run()
+//    println("readInts: ")
+//    readInts.run()
 }
