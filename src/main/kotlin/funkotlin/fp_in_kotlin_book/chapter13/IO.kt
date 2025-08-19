6d09bcdd7603829a6d19dc8a5b87bd4b6adc830b
kdzido <krzysztof.dzido@gmail.com>
2025-08-19 19:10:46 +0200

    [CHAP-13][LST-13.5-6] factorial REPL

--- src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter13/IO.kt
+++ src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter13/IO.kt
@@ -1,16 +1,30 @@
 package funkotlin.fp_in_kotlin_book.chapter13
 
+import arrow.Kind
 import funkotlin.fp_in_kotlin_book.chapter04.None
 import funkotlin.fp_in_kotlin_book.chapter04.Option
 import funkotlin.fp_in_kotlin_book.chapter04.Some
+import funkotlin.fp_in_kotlin_book.chapter11.Monad
 
-interface IO<A> {
+sealed class ForIO { companion object }
+typealias IOOf<A> = Kind<ForIO, A>
+inline fun <A> IOOf<A>.fix(): IO<A> = this as IO<A>
+
+data class IORef<A>(var value: A) {
+    fun set(a: A): IO<A> = IO { value = a; a }
+    fun get(): IO<A> = IO { value }
+    fun modify(f: (A) -> A): IO<A> = get().flatMap { a -> set(f(a)) }
+}
+
+interface IO<A> : IOOf<A> {
     companion object {
         fun <A> unit(a: () -> A) = object : IO<A> {
             override fun run(): A = a()
         }
 
         operator fun <A> invoke(a: () -> A) = unit(a)
+
+        fun ref(i: Int): IO<IORef<Int>> = IO { IORef(i) }
     }
 
     fun run(): A
@@ -75,9 +89,54 @@ val echo: IO<Unit> = stdin().flatMap(::stdout)
 val readInt: IO<Int> = stdin().map { it.toInt() }
 val readInts: IO<Pair<Int, Int>> = readInt assoc readInt
 
+val ioMonad: IOMonad = object : IOMonad {
+    override fun <A> unit(a: A): Kind<ForIO, A> =
+        IO { a }.fix()
+
+    override fun <A, B> flatMap(
+        fa: Kind<ForIO, A>,
+        f: (A) -> Kind<ForIO, B>,
+    ): Kind<ForIO, B> =
+        fa.fix().flatMap { a -> f(a).fix() }
+
+    override fun <A, B> map(
+        fa: Kind<ForIO, A>,
+        f: (A) -> B,
+    ): Kind<ForIO, B> =
+        fa.fix().flatMap { a -> unit(f(a)).fix() }
+}
+
+private fun factorial(n: Int): IO<Int> =
+    IO.ref(1).flatMap { acc: IORef<Int> ->
+        ioMonad.foreachM((1..n).toStream()) { i ->
+            acc.modify { it * i }.map { Unit }
+        }.fix().flatMap {
+            acc.get()
+        }
+    }
+
+private val help = """
+        | The Amazing Factorial REPL, v0.1
+        | q - quit
+        | <number> - compute the factorial of the given number
+        | <anything else> - bomb with horrible error
+        """.trimMargin("|")
+
+val factorialREPL: IO<Unit> =
+    ioMonad.sequenceDiscard(
+        IO { println(help) }.fix(),
+        ioMonad.doWhile(IO { readLine().orEmpty() }) { line ->
+            ioMonad.whenM(line != "q") {
+                factorial(line.toInt()).flatMap { n ->
+                    IO { println("factorial: $n") }
+                }
+            }
+        }.fix()
+    ).fix()
+
 fun main() {
 //    converter()
-    converter2().run()
+//    converter2().run()
 
 //    println("echo: ")
 //    echo.run()
@@ -85,4 +144,6 @@ fun main() {
 //    readInt.run()
 //    println("readInts: ")
 //    readInts.run()
+
+    factorialREPL.run()
 }
---
+++ src/main/kotlin/funkotlin/fp_in_kotlin_book/chapter13/IOMonad.kt
@@ -0,0 +1,76 @@
+package funkotlin.fp_in_kotlin_book.chapter13
+
+import arrow.Kind
+import funkotlin.fp_in_kotlin_book.chapter05.Cons
+import funkotlin.fp_in_kotlin_book.chapter05.Empty
+import funkotlin.fp_in_kotlin_book.chapter05.Stream
+import funkotlin.fp_in_kotlin_book.chapter05.fix
+import funkotlin.fp_in_kotlin_book.chapter11.Monad
+
+interface IOMonad : Monad<ForIO> {
+
+    fun <A> doWhile(
+        fa: IOOf<A>,
+        cond: (A) -> IOOf<Boolean>
+    ): IOOf<Unit> =
+        fa.fix().flatMap { a: A ->
+            cond(a).fix().flatMap<Unit> { ok: Boolean ->
+                if (ok) doWhile(fa, cond).fix() else unit(Unit).fix()
+            }
+        }
+
+    fun <A, B> forever(fa: IOOf<A>): IOOf<B> {
+        val t: IOOf<B> by lazy { forever<A, B>(fa) }
+        return fa.fix().flatMap { t.fix() }
+    }
+
+    fun <A, B> foldM(
+        sa: Stream<A>,
+        z: B,
+        f: (B, A) -> IOOf<B>,
+    ): IOOf<B> =
+        when (sa) {
+            is Cons -> f(z, sa.head()).fix().flatMap { b ->
+                foldM(sa.tail(), z, f).fix()
+            }
+            is Empty -> unit(z)
+        }
+
+    fun <A, B> foldDiscardM(
+        sa: Stream<A>,
+        z: B,
+        f: (B, A) -> Kind<ForIO, B>,
+    ): Kind<ForIO, Unit> =
+        foldM(sa, z, f).fix().map { Unit }
+
+    fun <A> foreachM(
+        sa: Stream<A>,
+        f: (A) -> IOOf<Unit>,
+    ): IOOf<Unit> =
+        foldDiscardM(sa, Unit) { _, a -> f(a)}
+
+    fun <A> skip(fa: Kind<ForIO, A>): Kind<ForIO, Unit> = map(fa) { Unit }
+
+    fun <A> sequenceDiscard(sa: Stream<Kind<ForIO, A>>): Kind<ForIO, Unit> =
+        foreachM(sa) { a -> skip(a) }
+
+    fun <A> sequenceDiscard(vararg fa: Kind<ForIO, A>): Kind<ForIO, Unit> =
+        sequenceDiscard(Stream.of(*fa))
+
+    fun <A> whenM(
+        ok: Boolean,
+        f: () -> Kind<ForIO, A>,
+    ): Kind<ForIO, Boolean> =
+        if (ok) f().fix().map { true } else unit(false)
+}
+
+fun IntRange.toStream(): Stream<Int> {
+    fun stream(from: Int, to: Int): Stream<Int> =
+        when (from) {
+            this.last + 1 ->
+                Stream.empty()
+            else ->
+                Stream.cons({ from }, { stream(from + 1, to) })
+        }
+    return stream(this.first, this.last)
+}
